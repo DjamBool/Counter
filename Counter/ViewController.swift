6ee13e9cafdcb91874a8b2aea89071d09e9f88eb
DjamBool <mailforgit2023@gmail.comu>
2023-06-21 11:39:09 +0300

    Basic task: added outlets for UILabel and UIButton o the ViewController, changed cornerRadius

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var counterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countLabel.layer.masksToBounds = true
+        countLabel.layer.cornerRadius = 10
+        counterButton.layer.cornerRadius = 10
     }
 
 
