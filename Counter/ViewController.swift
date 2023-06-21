596cffc2e84c3eccae48fd0076084b172e3e3efc
DjamBool <mailforgit2023@gmail.comu>
2023-06-21 13:33:16 +0300

    Complicated task: added buttons "+", "-", "reset"; added its functions

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,18 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var plusButtonTitle: UIButton!
     
-    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var resetButtonTitle: UIButton!
+    
+    @IBOutlet weak var minusButtonTitle: UIButton!
+    
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        countLabel.layer.masksToBounds = true
-        countLabel.layer.cornerRadius = 10
-        counterButton.layer.cornerRadius = 10
+        textLabel.layer.masksToBounds = true
+        textLabel.layer.cornerRadius = 10
     }
 
+    override func viewWillLayoutSubviews() {
+                setButtonAppearance(button: plusButtonTitle)
+                setButtonAppearance(button: minusButtonTitle)
+                setButtonAppearance(button: resetButtonTitle)
+    }
+    @IBAction func increaseCount() {
+        count += 1
+        textLabel.text = "Значение счётчика: \(String(count))"
+    }
+    
+    
+    @IBAction func reduceCount() {
+        if count > 0 {
+        count -= 1
+        textLabel.text = "Значение счётчика: \(String(count))"
+        } else {
+            textLabel.text = "Значение счётчика: 0"
+        }
+    }
+    
+    @IBAction func reset() {
+        count = 0
+        textLabel.text = "0"
+    }
+    
+    
+    func setButtonAppearance(button: UIButton) {
+        button.layer.cornerRadius = 10
+        button.titleLabel!.font =  UIFont(name: "HelveticaNeue-Thin", size: 40)
 
+    }
 }
 
