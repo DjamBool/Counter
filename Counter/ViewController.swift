4c3bf215da4291d2896c0ae6e708cc8f29fa9b58
DjamBool <mailforgit2023@gmail.comu>
2023-06-21 15:20:02 +0300

    Complicated task: added display of the history of changes in counter values

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,58 +7,65 @@
 
 import UIKit
 
+@available(iOS 15, *)
 class ViewController: UIViewController {
 
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var plusButtonTitle: UIButton!
-    
     @IBOutlet weak var resetButtonTitle: UIButton!
-    
     @IBOutlet weak var minusButtonTitle: UIButton!
-    
-    
     @IBOutlet weak var historyOfChangesTextView: UITextView!
     
     var count = 0
-    
+    let date = Date.now.formatted(date: .numeric, time: .standard)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         textLabel.layer.masksToBounds = true
         textLabel.layer.cornerRadius = 10
         historyOfChangesTextView.isEditable = false
+        historyOfChangesTextView.text = "История изменений:\n"
     }
 
     override func viewWillLayoutSubviews() {
                 setButtonAppearance(button: plusButtonTitle)
                 setButtonAppearance(button: minusButtonTitle)
                 setButtonAppearance(button: resetButtonTitle)
+        
     }
     @IBAction func increaseCount() {
         count += 1
         textLabel.text = "Значение счётчика: \(String(count))"
+       // historyOfChangesTextView.textAlignment = .left
+        historyOfChangesTextView.text += "[\(date)]: счетчик увеличен на 1\n"
     }
-    
     
     @IBAction func reduceCount() {
         if count > 0 {
         count -= 1
         textLabel.text = "Значение счётчика: \(String(count))"
+        historyOfChangesTextView.text += "[\(date)]: счетчик уменьшен на 1\n"
         } else {
             textLabel.text = "Значение счётчика: 0"
+            historyOfChangesTextView.text += "[\(date)]: попытка уменьшить значение счётчика ниже 0\n"
         }
     }
     
     @IBAction func reset() {
         count = 0
         textLabel.text = "0"
+        historyOfChangesTextView.text = "История изменений:\n[\(date)]: значение сброшено\n"
     }
     
-    
-    func setButtonAppearance(button: UIButton) {
+   private func setButtonAppearance(button: UIButton) {
         button.layer.cornerRadius = 10
         button.titleLabel!.font =  UIFont(name: "HelveticaNeue-Thin", size: 40)
 
     }
+
+    
+
+    
+  
 }
 
